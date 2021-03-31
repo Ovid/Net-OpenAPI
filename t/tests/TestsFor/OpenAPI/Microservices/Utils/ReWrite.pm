870fee11d7a43c0b09c040d2e36e068c4bf791bc
Ovid <curtis_ovid_poe@yahoo.com>
2021-03-31 11:48:27 +0200

    Make document rewriting even safer.
    We can now rewrite a rewritable document with another rewritable document.

--- t/tests/TestsFor/OpenAPI/Microservices/Utils/ReWrite.pm
+++ t/tests/TestsFor/OpenAPI/Microservices/Utils/ReWrite.pm
@@ -3,7 +3,6 @@ package TestsFor::OpenAPI::Microservices::Utils::ReWrite;
 # vim: textwidth=200
 
 use Test::Class::Moose extends => 'Test::OpenAPI::Microservices';
-use OpenAPI::Microservices::Utils::ReWrite;
 use OpenAPI::Microservices::Utils::Core ':all';
 
 sub test_rewrite {
@@ -15,17 +14,18 @@ It does me no injury for my neighbour to say there are 20 gods or no God. It nei
 — Thomas Jefferson
 END
 
-    ok my $rewrite = OpenAPI::Microservices::Utils::ReWrite->new( new_text => $sample ), 'We should be able to create a rewrite object without old text';
+    ok my $rewrite = $test->class_name->new( new_text => $sample ), 'We should be able to create a rewrite object without old text';
 
     my $expected = <<'END';
 #<<<: do not touch any code between this and the end comment. Checksum: da4a8adfd9ad75321c955e0d226532a6
-
 It does me no injury for my neighbour to say there are 20 gods or no God. It neither picks my pocket nor breaks my leg.
 
 — Thomas Jefferson
-
 #>>>: do not touch any code between this and the start comment. Checksum: da4a8adfd9ad75321c955e0d226532a6
 END
+
+    # saving this for use later
+    my $full_document_with_before_and_after_text = "this is before\n$expected\nthis is after";
 
     my $rewritten = $rewrite->rewritten;
     is $rewritten, $expected, '... and we should get our rewritten text back with start and end markers';
@@ -40,27 +40,48 @@ fondamentale qui fait passer l'homme de la position de sujet au statut de
 citoyen.
 END
 
-    ok $rewrite = OpenAPI::Microservices::Utils::ReWrite->new(
+    ok $rewrite = $test->class_name->new(
         old_text => $rewritten,
         new_text => $new_text,
-    ), 'We should be able to rewrite the old text with new text, but leaving "outside" areas unchanged';
+      ),
+      'We should be able to rewrite the old text with new text, but leaving "outside" areas unchanged';
 
-	$expected = <<'END';
+    $expected = <<'END';
 before
 
 #<<<: do not touch any code between this and the end comment. Checksum: 24e8541b178780253dcddeb0d5883e19
-
 Ces principes complètent ou précisent les valeurs exprimées par la devise de
 la République française : « Liberté, égalité, fraternité ». Reconnue comme un
 droit de l'homme par la Déclaration du 26 août 1789, la liberté est la valeur
 fondamentale qui fait passer l'homme de la position de sujet au statut de
 citoyen.
-
 #>>>: do not touch any code between this and the start comment. Checksum: 24e8541b178780253dcddeb0d5883e19
 
 after
 END
-    $test->is_multiline_text( $rewrite->rewritten, $expected, '... and get our new text as expected');
+    $rewritten = $rewrite->rewritten;
+    $test->is_multiline_text( $rewritten, $expected, '... and get our new text as expected' );
+
+    ok $rewrite = $test->class_name->new(
+        old_text => $rewritten,
+        new_text => $full_document_with_before_and_after_text,
+      ),
+      'We should be able to rewrite a document with a "full" new document, only extracting the rewrite portion of the new document.';
+    $rewritten = $rewrite->rewritten;
+
+    $expected = <<'END';
+before
+
+#<<<: do not touch any code between this and the end comment. Checksum: da4a8adfd9ad75321c955e0d226532a6
+It does me no injury for my neighbour to say there are 20 gods or no God. It neither picks my pocket nor breaks my leg.
+
+— Thomas Jefferson
+#>>>: do not touch any code between this and the start comment. Checksum: da4a8adfd9ad75321c955e0d226532a6
+
+after
+END
+
+    $test->is_multiline_text( $rewritten, $expected, '... and see only the part between checksums is replaced' );
 }
 
 1;
