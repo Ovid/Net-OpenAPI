7c06b1daa468081d6ba92f781485f81d445f8a62
Ovid <curtis_ovid_poe@yahoo.com>
2021-04-01 11:01:54 +0200

    Add a debug mode to our Endpoint generator.

--- lib/Net/OpenAPI/App/Endpoint.pm
+++ lib/Net/OpenAPI/App/Endpoint.pm
@@ -17,6 +17,7 @@ use Sub::Name;
 use Sub::Install 'install_sub';
 
 sub import {
+    my ( $class, %arg_for ) = @_;
     my $caller   = caller;
     state $check = compile(HTTPMethod, OpenAPIPath);
     my $endpoint = sub {
@@ -24,11 +25,11 @@ sub import {
         my ( $http_method, $path ) = split /\s+/ => trim($name), 2;
         $http_method = lc $http_method;
         ($http_method, $path) = $check->($http_method, $path);
-        my ( undef, $function_name, undef ) = resolve_method(
-            'X',
+        my ( $function_name, undef ) = resolve_method(
             $http_method,
             $path,
         );
+        say STDERR "# Installing endpoint '$name' into $caller as '$function_name'" if $arg_for{debug};
         subname "endpoint: $name" => $sub;
         install_sub(
             {
@@ -66,5 +67,17 @@ __END__
 
 =head1 DESCRIPTION
 
-This module automatically exports one function, C<endpoint>. This function takes a string
+This module automatically exports one function, C<endpoint>. This function
+takes a string matching an HTTP method and an OpenAPI path and installs a sub
+that C<Net::OpenAPI::App::Router> knows how to call.
+
+If you are developing with this and need to debug it, use this:
+
+    use Net::OpenAPI::App::Endpoint debug => 1;
+
+That will print to C<STDERR> each endpoint and the sub it is mapped to.
+
+	# Installing endpoint 'get /pet/findByStatus' into My::OpenAPI::Model::Pet as 'get_findByStatus'
+	# Installing endpoint 'get /pet/findByTags' into My::OpenAPI::Model::Pet as 'get_findByTags'
+	# Installing endpoint 'get /pet/{petId}' into My::OpenAPI::Model::Pet as 'with_args_get'
 
