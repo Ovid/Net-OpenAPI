8a6d98d3ca0cc298c2e099755c017b635e6fb150
Ovid <curtis_ovid_poe@yahoo.com>
2021-04-08 08:08:18 +0200

    Update router tests to match new calling convention.

--- t/tests/TestsFor/Net/OpenAPI/App/Router.pm
+++ t/tests/TestsFor/Net/OpenAPI/App/Router.pm
@@ -1,31 +1,25 @@
 package TestsFor::Net::OpenAPI::App::Router;
 
+use Plack::Request;
 use Net::OpenAPI::Policy;
 use Test::Class::Moose extends => 'Test::Net::OpenAPI';
 
 sub test_routing {
     my $test   = shift;
-    my $router = $test->class_name->new;
     my @routes = (
-        { path => '/pet',              http_method => 'post',   dispatch_to => 'My::Project::OpenAPI::Model::Pet', action => 'post' },
-        { path => '/pet',              http_method => 'put',    dispatch_to => 'My::Project::OpenAPI::Model::Pet', action => 'put' },
-        { path => '/pet/findByStatus', http_method => 'get',    dispatch_to => 'My::Project::OpenAPI::Model::Pet', action => 'get_findByStatus' },
-        { path => '/pet/findByTags',   http_method => 'get',    dispatch_to => 'My::Project::OpenAPI::Model::Pet', action => 'get_findByTags' },
-        { path => '/pet/{petId}',      http_method => 'delete', dispatch_to => 'My::Project::OpenAPI::Model::Pet', action => 'with_args_delete' },
-        { path => '/pet/{petId}',      http_method => 'get',    dispatch_to => 'My::Project::OpenAPI::Model::Pet', action => 'with_args_get' },
-        { path => '/pet/{petId}',      http_method => 'post',   dispatch_to => 'My::Project::OpenAPI::Model::Pet', action => 'with_args_post' },
-        {
-            path   => '/pet/{petId}/uploadImage', http_method => 'post', dispatch_to => 'My::Project::OpenAPI::Model::Pet',
-            action => 'with_args_post___uploadImage'
-        },
-        { path => '/日本/{city}', http_method => 'get', dispatch_to => 'My::Project::OpenAPI::Model::RiBen', action => 'with_args_get' },
+        { path => '/pet', http_method => 'post', controller => 'My::Project::OpenAPI::Model::Pet', method => 'post' },
+        { path => '/pet', http_method => 'post', controller => 'My::Project::OpenAPI::Model::Pet', method => 'post' },
     );
-    foreach my $route (@routes) {
-        my ( $method, $path ) = @{$route}{qw/http_method path/};
-        ok $router->add_route($route), "We should be able to add routes to our router: $method $path";
-    }
-    throws_ok { $router->add_route( $routes[0] ) }
-    qr{Route for POST /pet already added},
+    my $router = $test->class_name->new( routes => \@routes );
+    my $env    = {
+        QUERY_STRING   => 'query=%82%d9%82%b0',    # <= encoded by 'cp932'
+        REQUEST_METHOD => 'GET',
+        HTTP_HOST      => 'example.com',
+        PATH_INFO      => '/get',
+    };
+    my $req = Plack::Request->new($env);
+    throws_ok { $router->match($req) }
+    qr{Route for POST /pet already registered},
       'Trying to add a duplicate route should fail';
 }
 
@@ -36,23 +30,18 @@ sub test_match {
         sub foo { return 42 }
     }
     $INC{'Example/Package/For/Dispatch.pm'} = 1;    # fake loading it
-    my $router = $test->class_name->new;
     my @routes = (
-        { path => '/get', http_method => 'get', dispatch_to => 'Example::Package::For::Dispatch', action => 'foo' },
-        { path => '/put', http_method => 'put', dispatch_to => 'Example::Package::For::Dispatch', action => 'no_such_sub' },
-        { path => '/foo', http_method => 'get', dispatch_to => 'No::Such::Package::Is::Here',     action => 'no_such_sub' },
+        { path => '/get', http_method => 'get', controller => 'Example::Package::For::Dispatch', method => 'foo' },
+        { path => '/put', http_method => 'put', controller => 'Example::Package::For::Dispatch', method => 'no_such_sub' },
+        { path => '/foo', http_method => 'get', controller => 'No::Such::Package::Is::Here',     method => 'no_such_sub' },
     );
-    foreach my $route (@routes) {
-        my ( $method, $path ) = @{$route}{qw/http_method path/};
-        ok $router->add_route($route), "We should be able to add routes to our router: $method $path";
-    }
-    my $env = {
+    my $router = $test->class_name->new( routes => \@routes );
+    my $env    = {
         QUERY_STRING   => 'query=%82%d9%82%b0',     # <= encoded by 'cp932'
         REQUEST_METHOD => 'GET',
         HTTP_HOST      => 'example.com',
         PATH_INFO      => '/get',
     };
-    use Plack::Request;
     my $req   = Plack::Request->new($env);
     my $match = $router->match($req);
     is $match->{dispatch}->($req), 42, 'We should be able to dispatch directly to subs';
@@ -87,22 +76,17 @@ sub test_ambiguous_matches {
         endpoint 'get /pet/{petId}'      => sub {'with_args_get'};
     }
     $INC{'Example/Package/For/Dispatch.pm'} = 1;    # fake loading it
-    my $router = $test->class_name->new;
     my @routes = (
-        { path => '/pet/findByStatus', http_method => 'get', dispatch_to => 'Example::Package::For::Dispatch', action => 'get_findByStatus' },
-        { path => '/pet/findByTags',   http_method => 'get', dispatch_to => 'Example::Package::For::Dispatch', action => 'get_findByTags' },
-        { path => '/pet/{petId}',      http_method => 'get', dispatch_to => 'Example::Package::For::Dispatch', action => 'with_args_get' },
+        { path => '/pet/findByStatus', http_method => 'get', controller => 'Example::Package::For::Dispatch', method => 'get_findByStatus' },
+        { path => '/pet/findByTags',   http_method => 'get', controller => 'Example::Package::For::Dispatch', method => 'get_findByTags' },
+        { path => '/pet/{petId}',      http_method => 'get', controller => 'Example::Package::For::Dispatch', method => 'with_args_get' },
     );
-    foreach my $route (@routes) {
-        my ( $method, $path ) = @{$route}{qw/http_method path/};
-        ok $router->add_route($route), "We should be able to add routes to our router: $method $path";
-    }
-    my $env = {
+    my $router = $test->class_name->new( routes => \@routes );
+    my $env    = {
         REQUEST_METHOD => 'GET',
         HTTP_HOST      => 'example.com',
         PATH_INFO      => '/pet/3',
     };
-    use Plack::Request;
     my $req   = Plack::Request->new($env);
     my $match = $router->match($req);
     is $match->{dispatch}->($req), 'with_args_get', 'We should be able to dispatch directly to subs';
