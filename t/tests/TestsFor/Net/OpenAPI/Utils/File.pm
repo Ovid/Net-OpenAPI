a291bbab3a236d7b905e928a0e6d6a64057529f1
Ovid <curtis_ovid_poe@yahoo.com>
2021-04-08 08:45:43 +0200

    Stub tests for our file utilities

--- t/tests/TestsFor/Net/OpenAPI/Utils/File.pm
+++ t/tests/TestsFor/Net/OpenAPI/Utils/File.pm
@@ -6,40 +6,13 @@ use Net::OpenAPI::Policy;
 use Test::Class::Moose extends => 'Test::Net::OpenAPI';
 use Net::OpenAPI::Utils::File qw(:all);
 
-sub test_unindent {
-    my $test = shift;
-    my $code = unindent(<<"    END");
-    package Some::Package;
-    
-    use v5.16.0;
-    use strict;
-    use warnings;
-        # keep this indentation
-      # and this
-    END
-    my $expected = <<"END";
-package Some::Package;
-
-use v5.16.0;
-use strict;
-use warnings;
-    # keep this indentation
-  # and this
-END
-    is $code, $expected, 'unindent() should properly unindent our code';
-
-    my $bad_indent = <<"    END";
-    package Some::Package;
-    
-  use v5.16.0;
-    use strict;
-    use warnings;
-        # keep this indentation
-      # and this
-    END
-    throws_ok { unindent($bad_indent) }
-      qr/\Qunindent() failed with line found with indentation less than '4'/,
-      'Unindenting anything with a line whose leading whitespaces are less than first line should fail';
+sub test_utils {
+    my $test  = shift;
+    my $stuff = slurp('data/v3-petstore.json');
+    ok $stuff, 'We should be able to read files';
+    throws_ok { slurp('no such file') }
+    qr/No such file or directory/,
+      '... but trying to slurp a non-existent file should fail';
 }
 
 __PACKAGE__->meta->make_immutable;
