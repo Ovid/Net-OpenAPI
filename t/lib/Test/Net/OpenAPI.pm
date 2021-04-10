3e68f88faf485a534c2ef6737653fc2eed2cc3fa
Ovid <curtis_ovid_poe@yahoo.com>
2021-04-10 09:33:16 +0200

    Document is_multiline_text() method

--- t/lib/Test/Net/OpenAPI.pm
+++ t/lib/Test/Net/OpenAPI.pm
@@ -8,6 +8,16 @@ use Test::Class::Moose;
 use Net::OpenAPI::Policy;
 with 'Test::Class::Moose::Role::AutoUse';
 
+=head2 C<is_multiline_text>
+
+    $test->is_multiline_text($have, $want, $message);
+
+Test which passes if C<$have> and C<$want> are the same. however, we split
+the texts on newlines and use C<eq_or_diff> to make it easier to spot where
+the differences are.
+
+=cut
+
 sub is_multiline_text {
     my ( $test, $text, $expected, $message ) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
